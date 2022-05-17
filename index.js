bda79a9062e6c3d7d001d24e3c476d2e110edc04
Ulrike Werner <ulrike_werner@web.de>
2022-05-17 12:08:58 +0200

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+export function greet(name) {
+  if (name === "Jessica" || name === "Thomas" || name === "Stefan") {
+    return "Hello Coach!";
+  } else if (name === "") {
+    return "Hello stranger!";
+  } else {
+    return "Hello " + name + " !";
+  }
+}
